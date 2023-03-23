68d2b2d16e17601d77538d0fc5961c9e406059a8
F3neden <20224914+F3neden@users.noreply.github.com>
2023-03-23 11:29:54 +0100

    get center of playground

--- detectPlayground.py
+++ detectPlayground.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
-
-#Detect Playground
+"""
+Detect Playground
+"""
+## preprocessing
 img = cv2.imread('data/empty.JPG')
 rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 upper_red = np.array([255, 70, 70])
@@ -9,25 +11,22 @@ lower_red = np.array([100, 0, 0])
 
 mask = cv2.inRange(rgb, lower_red, upper_red)
 img_with_mask = cv2.bitwise_and(img, img, mask = mask)
-cv2.imshow("IMG",img_with_mask)
-cv2.waitKey(0)
-
 gray_img = cv2.cvtColor(img_with_mask, cv2.COLOR_BGR2GRAY)
 
+## contour detection
 contours, hierarchy = cv2.findContours(gray_img, 1, 2)
 print("Number of contours detected:", len(contours))
 
+## get second largest square (largest is border of image)
 index, area = sorted([[index, cv2.contourArea(cnt)] for index, cnt in enumerate(contours)], reverse=True)[1]
-
-# Get Center
 cnt = contours[index]
 
-# define main island contour approx. and hull
+## define playground borders
 perimeter = cv2.arcLength(cnt,True)
 epsilon = 0.01*cv2.arcLength(cnt,True)
 approx = cv2.approxPolyDP(cnt,epsilon,True)
 
-cv2.drawContours(img, [approx], -1, (0, 0, 255), 6)
-
-cv2.imshow("Contour", img)
-cv2.waitKey(0)
+## get center of playground
+x = [p[0][0] for p in approx]
+y = [p[0][1] for p in approx]
+center = (sum(x) / len(approx), sum(y) / len(approx))
