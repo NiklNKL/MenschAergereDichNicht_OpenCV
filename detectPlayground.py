d7ff74e790ecb1d9fcc586a650952875dcba9304
Momo <mohamed.elbahar98@gmail.com>
2023-03-23 11:08:55 +0100

    found playground corners

--- detectPlayground.py
+++ detectPlayground.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+#Detect Playground
 img = cv2.imread('data/empty.JPG')
 rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 upper_red = np.array([255, 70, 70])
@@ -17,7 +18,16 @@ contours, hierarchy = cv2.findContours(gray_img, 1, 2)
 print("Number of contours detected:", len(contours))
 
 index, area = sorted([[index, cv2.contourArea(cnt)] for index, cnt in enumerate(contours)], reverse=True)[1]
-img = cv2.drawContours(img, [contours[index]], -1, (0,255,255), 10)
 
-cv2.imshow("Shapes", img)
+# Get Center
+cnt = contours[index]
+
+# define main island contour approx. and hull
+perimeter = cv2.arcLength(cnt,True)
+epsilon = 0.01*cv2.arcLength(cnt,True)
+approx = cv2.approxPolyDP(cnt,epsilon,True)
+
+cv2.drawContours(img, [approx], -1, (0, 0, 255), 6)
+
+cv2.imshow("Contour", img)
 cv2.waitKey(0)
