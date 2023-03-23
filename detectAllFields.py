517a644c28d913666eec44c1bafd0b91db1d7a4b
Momo <mohamed.elbahar98@gmail.com>
2023-03-23 16:08:21 +0100

    playground detection

--- detectAllFields.py
+++ detectAllFields.py
@@ -1,80 +1,116 @@
 import cv2
+import math
 import numpy as np
+from detectPlayground import get_playground
 
-# Read image.
-img = cv2.imread('/Users/jan/Library/CloudStorage/OneDrive-bib&FHDW/Wissenschaftliche Arbeit/2.4 CS/brett.png', cv2.IMREAD_COLOR)
+MINR_AREA_RATIO = 8.670466504392528e-06
 
+def get_angle(a, b, c):
+    ang = math.degrees(math.atan2(c[1]-b[1], c[0]-b[0]) - math.atan2(a[1]-b[1], a[0]-b[0]))
+    return ang + 360 if ang < 0 else ang
 
-# get current positions of all trackbars
-hMin = cv2.getTrackbarPos('HMin','image')
-sMin = cv2.getTrackbarPos('SMin','image')
-vMin = cv2.getTrackbarPos('VMin','image')
+def get_street(img, area):
+	# blur
+	blurred = cv2.medianBlur(img, 7)
 
-hMax = cv2.getTrackbarPos('HMax','image')
-sMax = cv2.getTrackbarPos('SMax','image')
-vMax = cv2.getTrackbarPos('VMax','image')
-
-# Set minimum and max HSV values to display
-lower = np.array([hMin, sMin, vMin])
-upper = np.array([hMax, sMax, vMax])
-
-# invert
-inverted = cv2.bitwise_not(img)
-# blur
-blurred = cv2.medianBlur(inverted, 7)
-# convert to gray scale for HoughCircles
-gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-
-# Apply Hough transform on the blurred image.
-## detect all circles
-# detected_circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 50, param1 = 50, param2 = 45, minRadius = 10, maxRadius = 120)
-
-## detect "street"
-# detected_circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 50, param1 = 50, param2 = 45, minRadius = 50, maxRadius = 120)
-
-## detect "house" for beginning (rectangle) and end (line)
-# convert to HSV
-imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-blurred_houses = cv2.medianBlur(imgHSV, 7)
-# ### for red:
-# mask = cv2.inRange(blurred_houses, np.array([0, 100, 100]), np.array([10, 255, 255]) )
-
-### for green:
-mask = cv2.inRange(blurred_houses, np.array([40, 100, 100]), np.array([70, 255, 255]) )
-
-### for yellow: (not working)
-# mask = cv2.inRange(blurred_houses, )
-
-### for black: (not working)
-# blurredHouse = cv2.medianBlur(img, 7)
-# # grayHouse = cv2.cvtColor(blurredHouse, cv2.COLOR_BGR2GRAY)
-# gray = cv2.cvtColor(blurredHouse, cv2.COLOR_BGR2GRAY)
-# mask = cv2.inRange(gray, 0, 40)
+	# convert to gray scale for HoughCircles
+	gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
 
 
+	## detect "street"
+	detected_circles = None
+	minR = round(MINR_AREA_RATIO * area + 5)
+	while(detected_circles is None):
+		print(f"radius: {minR}")
+		circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 50, param1 = 120, param2 = 45, minRadius = minR, maxRadius = 200)
+		if circles is not None:
+			print(circles.shape)
+			if circles.shape[1] == 40:
+				detected_circles = circles
+				break
+		minR -=5
+	return np.squeeze(detected_circles, axis=0)
 
-# # mask = cv2.inRange(blurred_houses, np.array([0, 100, 100]), np.array([10, 255, 255]) )
-gray =cv2.bitwise_and(img, img, mask = mask)
-# cv2.imshow("Detected Circle", gray)
-# # cv2.waitKey(0)
+def get_start_fields(corners, center, street):
+	
+	distances = []
+	for index, field in enumerate(street):
+		val = math.dist(field[:2],center)
+		distances.append([index,val])
+	distances = sorted(distances,key=lambda c: c[1])
 
-# detected_circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 40, param1 = 100, param2 = 20, minRadius = 30, maxRadius = 40)
-detected_circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 40, param1 = 100, param2 = 10, minRadius = 30, maxRadius = 40)
-""""""
-# Draw circles that are detected.
-# if detected_circles is not None:
+	corner = corners[0]
+	angles = []
+	for index, field in distances:
+		angle = get_angle(corner, center, field)
+		print(angle)
+		angles.append([index, angle])
+	angles = sorted(angles,key=lambda c: c[1])
+	
 
-# Convert the circle parameters a, b and r to integers.
-detected_circles = np.uint16(np.around(detected_circles))
+	
+	return distances[-8:]
+	
 
-for pt in detected_circles[0, :]:
-	a, b, r = pt[0], pt[1], pt[2]
 
-	# Draw the circumference of the circle.
-	cv2.circle(img, (a, b), r, (0, 255, 0), 2)
+if __name__ == "__main__":
+	# Read image.
+	img = cv2.imread('data/empty.JPG', cv2.IMREAD_COLOR)
 
-	# Draw a small circle (of radius 1) to show the center.
-	cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
-cv2.imshow("Detected Circle", img)
-cv2.waitKey(0)
+	corners, center, area = get_playground(img)
 
+	street = get_street(img, area)
+
+	fields = get_start_fields(corners, center, street)
+
+	print(len(fields))
+
+
+	# detected_circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 50, param1 = 120, param2 = 45, minRadius = 115, maxRadius = 200)
+
+
+	## detect "house" for beginning (rectangle) and end (line)
+	# convert to HSV
+	imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	blurred_houses = cv2.medianBlur(imgHSV, 7)
+	# ### for red:
+	# mask = cv2.inRange(blurred_houses, np.array([0, 100, 100]), np.array([10, 255, 255]) )
+
+	### for green:
+	mask = cv2.inRange(blurred_houses, np.array([40, 100, 100]), np.array([70, 255, 255]) )
+
+	### for yellow: (not working)
+	# mask = cv2.inRange(blurred_houses, )
+
+	### for black: (not working)
+	# blurredHouse = cv2.medianBlur(img, 7)
+	# # grayHouse = cv2.cvtColor(blurredHouse, cv2.COLOR_BGR2GRAY)
+	# gray = cv2.cvtColor(blurredHouse, cv2.COLOR_BGR2GRAY)
+	# mask = cv2.inRange(gray, 0, 40)
+
+
+
+	# # mask = cv2.inRange(blurred_houses, np.array([0, 100, 100]), np.array([10, 255, 255]) )
+	gray =cv2.bitwise_and(img, img, mask = mask)
+	# cv2.imshow("Detected Circle", gray)
+	# # cv2.waitKey(0)
+
+	# detected_circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 40, param1 = 100, param2 = 20, minRadius = 30, maxRadius = 40)
+	# detected_circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 40, param1 = 100, param2 = 10, minRadius = 30, maxRadius = 40)
+	""""""
+	# Draw circles that are detected.
+	# if detected_circles is not None:
+
+	# Convert the circle parameters a, b and r to integers.
+	street = np.uint16(np.around(street))
+
+	for pt in street[0, :]:
+		a, b, r = pt[0], pt[1], pt[2]
+
+		# Draw the circumference of the circle.
+		cv2.circle(img, (a, b), r, (0, 255, 0), 20)
+
+		# Draw a small circle (of radius 1) to show the center.
+		cv2.circle(img, (a, b), 1, (0, 0, 255), 3)
+	cv2.imshow("Detected Circle", img)
+	cv2.waitKey(0)
--- detectPlayground.py
+++ detectPlayground.py
@@ -3,30 +3,32 @@ import numpy as np
 """
 Detect Playground
 """
-## preprocessing
-img = cv2.imread('data/empty.JPG')
-rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-upper_red = np.array([255, 70, 70])
-lower_red = np.array([100, 0, 0])
+def get_playground(img):
+    ## preprocessing
+    # img = cv2.imread('data/empty.JPG')
+    rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    upper_red = np.array([255, 70, 70])
+    lower_red = np.array([100, 0, 0])
 
-mask = cv2.inRange(rgb, lower_red, upper_red)
-img_with_mask = cv2.bitwise_and(img, img, mask = mask)
-gray_img = cv2.cvtColor(img_with_mask, cv2.COLOR_BGR2GRAY)
+    mask = cv2.inRange(rgb, lower_red, upper_red)
+    img_with_mask = cv2.bitwise_and(img, img, mask = mask)
+    gray_img = cv2.cvtColor(img_with_mask, cv2.COLOR_BGR2GRAY)
 
-## contour detection
-contours, hierarchy = cv2.findContours(gray_img, 1, 2)
-print("Number of contours detected:", len(contours))
+    ## contour detection
+    contours, _ = cv2.findContours(gray_img, 1, 2)
+    print("Number of contours detected:", len(contours))
 
-## get second largest square (largest is border of image)
-index, area = sorted([[index, cv2.contourArea(cnt)] for index, cnt in enumerate(contours)], reverse=True)[1]
-cnt = contours[index]
+    ## get second largest square (largest is border of image)
+    index, area = sorted([[index, cv2.contourArea(cnt)] for index, cnt in enumerate(contours)], reverse=True)[1]
+    cnt = contours[index]
 
-## define playground borders
-perimeter = cv2.arcLength(cnt,True)
-epsilon = 0.01*cv2.arcLength(cnt,True)
-approx = cv2.approxPolyDP(cnt,epsilon,True)
+    ## define playground borders
+    epsilon = 0.01*cv2.arcLength(cnt,True)
+    corners = np.squeeze(cv2.approxPolyDP(cnt,epsilon,True), axis=1)
 
-## get center of playground
-x = [p[0][0] for p in approx]
-y = [p[0][1] for p in approx]
-center = (sum(x) / len(approx), sum(y) / len(approx))
+    ## get center of playground
+    x = [p[0] for p in corners]
+    y = [p[1] for p in corners]
+    center = (sum(x) / len(corners), sum(y) / len(corners))
+
+    return (corners, center, area)
